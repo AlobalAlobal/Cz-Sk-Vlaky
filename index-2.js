21e3de9e471433b0c7e5dcd6fdead8f8b91f24b9
AlobalAlobal <matus.becak@gmail.com>
2023-07-12 18:05:08 +0200

    Added Nicknames an Descriptions

--- index-2.js
+++ index-2.js
@@ -2,80 +2,80 @@ const trains = [
   {
     type: 'electric',
     name: 'Elektrická lokomotíva 1',
-    description: 'Vlak 1',
-	nickname: 'Vlak',
+    description: 'Elektrická lokomotíva používaná na posunovanie a ľahkú traťovú službu, často sú používané ako staničné zálohy',
+	  nickname: '210 - Žehlička/Jazvec',
     image: 'Obrázky/Elektrika/210.jpg',
     link: 'Stranky-Vlaky/Elektrika-1.html'
   },
   {
     type: 'electric',
     name: 'Elektrická lokomotíva 2',
-    description: 'Vlak 2',
-	nickname: 'Vlak',
+    description: 'Laminátky sú univerzálne elektrické lokomotívy, dnes sa ale prevažne používajú v nákladnej doprave',
+	  nickname: '230/240 - Laminátka/Lamino',
     image: 'Obrázky/Elektrika/230.jpg',
     link: 'Stranky-Vlaky/Elektrika-2.html'
   },
   {
     type: 'electric',
-    name: 'Elektrická lokomotíva 10',
-    description: 'Vlak 10',
-	nickname: 'Vlak',
-    image: 'Obrázky/Elektrika/263.jpg',
-    link: 'Stranky-Vlaky/Elektrika-10.html'
-  },
-  {
-    type: 'electric',
     name: 'Elektrická lokomotíva 3',
-    description: 'Vlak 3',
-	nickname: 'Vlak',
+    description: 'Po technickej stránke sa jedná o pokračovanie rady 240 (nepriamo 230), používané hlavne v osobnej doprave',
+	  nickname: '242 - Plecháč/Plechovka',
     image: 'Obrázky/Elektrika/242.jpg',
     link: 'Stranky-Vlaky/Elektrika-3.html'
   },
   {
     type: 'electric',
-    name: 'Elektrická lokomotíva 9',
-    description: 'Vlak 9',
-	nickname: 'Vlak',
+    name: 'Elektrická lokomotíva 4',
+    description: 'Vyvrcholenie rady lokomotív x6x, aj napriek veľkej spoľahlivosti bolo vyrobených iba 12 kusov kde všetky patria ČD',
+	  nickname: '263 - Princezná',
+    image: 'Obrázky/Elektrika/263.jpg',
+    link: 'Stranky-Vlaky/Elektrika-10.html'
+  },
+  {
+    type: 'electric',
+    name: 'Elektrická lokomotíva 5',
+    description: 'Najrýchlejšia a najsilnejšia lokomotíva v ČSR, prevažne používaná na ťahanie Expresov, Rýchlikov a REX',
+	  nickname: '350 - Gorila/Krysa/Škodovácké zlato',
     image: 'Obrázky/Elektrika/350.jpg',
     link: 'Stranky-Vlaky/Elektrika-9.html'
   },
   {
     type: 'electric',
-    name: 'Elektrická lokomotíva 4',
-    description: 'Vlak 4',
-	nickname: 'Vlak',
+    name: 'Elektrická lokomotíva 6',
+    description: 'Prestavba starej rady 363 od ktorej sa delia vyššou maximálnou rýchlosťou, používaná sú hlavne v osobnej doprave',
+	  nickname: '362 - Eso/Rýchle Eso',
     image: 'Obrázky/Elektrika/362.jpg',
     link: 'Stranky-Vlaky/Elektrika-4.html'
   },
   {
     type: 'electric',
-    name: 'Elektrická lokomotíva 8',
-    description: 'Vlak 8',
-	nickname: 'Vlak',
+    name: 'Elektrická lokomotíva 7',
+    description: 'Povodné lokomotívy pochádzajú z Belgicka, v počte 9 kusov boli rekonštruované na použitie v nákladnej doprave',
+	  nickname: '365 - Belgičanka',
     image: 'Obrázky/Elektrika/365.jpg',
     link: 'Stranky-Vlaky/Elektrika-8.html'
   },
   {
     type: 'electric',
-    name: 'Elektrická lokomotíva 5',
-    description: 'Vlak 5',
-	nickname: 'Vlak',
+    name: 'Elektrická lokomotíva 8',
+    description: 'V počte 2 kusov modernizované z rady 380 pre Slovensko, používané hlavne v prímestskej doprave okolo Bratislavy',
+   	nickname: '380/381 - Emil Zátopek/Messerschmitt/Vaňa/Sváračka/Mrazák',
     image: 'Obrázky/Elektrika/381.jpg',
     link: 'Stranky-Vlaky/Elektrika-5.html'
   },
   {
     type: 'electric',
-    name: 'Elektrická lokomotíva 6',
-    description: 'Vlak 6',
-	nickname: 'Vlak',
+    name: 'Elektrická lokomotíva 9',
+    description: 'Modulárna lokomotíva používaná naprieč Európou a aj v USA, používaná v nákladnej aj v osobnej doprave - Expresy',
+	  nickname: '383 - Vectron, Vetroň, Vetrák',
     image: 'Obrázky/Elektrika/383.jpg',
     link: 'Stranky-Vlaky/Elektrika-6.html'
   },
   {
     type: 'electric',
-    name: 'Elektrická lokomotíva 7',
-    description: 'Vlak 7',
-	nickname: 'Vlak',
+    name: 'Elektrická lokomotíva 10',
+    description: 'Trojdielna súprava používaná v prímestskej doprave, postavená Škodou-Vagónkou a vyvážaná aj do zahraničia',
+  	nickname: '671 - Jánošík, Ešus, Hliník',
     image: 'Obrázky/Elektrika/671.jpg',
     link: 'Stranky-Vlaky/Elektrika-7.html'
   },
